894015e27bc0a76d930233fbf8e5e3adb5f1989d
Tim Kamanin <tim@timonweb.com>
2022-02-19 20:03:42 +0100

    Fixes Django 4.0 compability

--- wagtailsnippetscopy/wagtail_hooks.py
+++ wagtailsnippetscopy/wagtail_hooks.py
@@ -5,7 +5,11 @@ from .views import copy
 
 @hooks.register('register_admin_urls')
 def register_admin_urls():
-    from django.conf.urls import url
+    try:
+        from django.conf.urls import url
+    except ImportError:
+        from django.urls import re_path as url
+
     return [
         url(r'^snippets-copy/(\w+)/(\w+)/(\d+)/', copy, name='wagtailsnippetscopy_copy'),
     ]
