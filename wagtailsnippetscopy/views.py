d923af5d49ea6cbfacdd66147f12e29e6498205f
Erick Otenyo <otenyo.erick@gmail.com>
2023-03-30 15:39:31 +0300

    update wagtail hooks import

--- wagtailsnippetscopy/views.py
+++ wagtailsnippetscopy/views.py
@@ -13,7 +13,7 @@ from wagtail.admin import messages
 from wagtail.admin.auth import user_passes_test, user_has_any_page_permission, permission_denied
 from wagtail.admin.views.pages.utils import get_valid_next_url_from_request
 from wagtail.contrib.modeladmin.helpers import AdminURLHelper
-from wagtail.core import hooks
+from wagtail import hooks
 from wagtail.snippets.permissions import get_permission_name
 
 from .registry import snippet_copy_registry
